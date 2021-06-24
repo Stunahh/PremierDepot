02cae4afe45186a8e517bf9c8f12061e34e4db0a
Stunahh <86412196+Stunahh@users.noreply.github.com>
2021-06-24 16:50:28 +0200

    Delete maquettes directory

--- maquettes/js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let btnSubmit= document.getElementsByClassName('petite')[0];
-console.log(btnSubmit)
-
-btnSubmit.addEventListener("click",function(e){
-    e.preventDefault();
-
-});
