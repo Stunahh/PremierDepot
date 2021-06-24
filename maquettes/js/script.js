c95f5493333fd4e7b61da85a11a20ca3cdd98495
Malik <madlvdl@gmail.com>
2021-06-24 15:12:50 +0200

    ajout maquettes

---
+++ maquettes/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let btnSubmit= document.getElementsByClassName('petite')[0];
+console.log(btnSubmit)
+
+btnSubmit.addEventListener("click",function(e){
+    e.preventDefault();
+
+});
